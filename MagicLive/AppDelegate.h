238b44782ee9fb86670ec72d8cf93aad50807f8e
Shawn <1329980100@qq.com>
2019-07-22 18:59:28 +0800

    Initial Commit

---
+++ MagicLive/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MagicLive
+//
+//  Created by Shawn Wong on 2019/7/22.
+//  Copyright © 2019 ShawnWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MagicLive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MagicLive
+//
+//  Created by Shawn Wong on 2019/7/22.
+//  Copyright © 2019 ShawnWong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
